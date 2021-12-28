36ae7a1443130ec176844b5fc2dacf0c48e80d45
Shreyans Maini <shreyans@oneassure.in>
2021-12-28 16:34:57 +0530

    added webhook classes

--- src/onepipepy/webhook.py
+++ src/onepipepy/webhook.py
@@ -1,38 +1,59 @@
-class DealWebhook(object):
-    def __init__(self):
-        pass
-
-    def stage_change(self):
-        pass
-
-    def field_change(self):
-        pass
+from .errors import UnAuthorizedWebhook
+from .models import *
 
 
-class PersonWebhook(object):
-    def __init__(self):
-        pass
+class Added(object):
+    def __init__(self, webhook):
+        self.webhook = webhook
+        self.obj = self.webhook.event_obj[1]
+        current = self.webhook.json_data.get("current", dict())
+        setattr(self, self.obj, globals()[self.obj](**current))
 
-    def assign_owner(self):
-        pass
 
-    def get_source(self):
-        pass
+class Updated(object):
+    def __init__(self, webhook):
+        self.webhook = webhook
+        self.obj = self.webhook.event_obj[1]
+        current = self.webhook.json_data.get("current", dict())
+        previous = self.webhook.json_data.get("previous", dict())
+        for key in current.keys():
+            if current.get(key, None) != previous.get(key, None):
+                self.change = dict(
+                    key=key,
+                    current_value=current[key],
+                    previous_value=previous[key]
+                )
+                break
+        setattr(self, "current_" + self.obj, globals()[self.obj](**current))
+        setattr(self, "previous_" + self.obj, globals()[self.obj](**previous))
+
+
+class Merged(object):
+    def __init__(self, webhook):
+        self.webhook = webhook
+
+
+class Deleted(object):
+    def __init__(self, webhook):
+        self.webhook = webhook
 
 
 class Webhook(object):
-    def __init__(self):
-        self.auth()
-        self.parse()
+    def __init__(self, *args, **kwargs):
+        webhook_auth = kwargs.get("webhook_auth", None)
+        if webhook_auth is not None:
+            self.username = webhook_auth.get("username")
+            self.password = webhook_auth.get("password")
+            self.auth()
+        self.request = args[0]
+        self.json_data = self.request.get_json(force=True)
+        self.event_obj = self.json_data["event"].split(".")
+        setattr(self, self.event_obj[0], globals()[self.event_obj[0]](self))
 
     def auth(self):
-        pass
-
-    def parse(self):
-        '''
-
-        :return:
-        if the object is either deal or person webhook
-        '''
-        pass
+        auth = self.request.authorization
+        if auth is None:
+            raise UnAuthorizedWebhook
+        if auth["username"] != self.username and auth["password"] != self.password:
+            raise UnAuthorizedWebhook
 
